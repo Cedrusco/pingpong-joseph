42341ba41800ce646a672d7a1f85175420e3e631
Joseph Lionarons <joseph.lionarons@cedrus.digital>
2020-03-24 16:04:58 -0400

    initial commit

---
+++ src/main/java/com/cedrus/aeolion/kafkaspringpong/KafkaspringpongApplication.java
@@ -0,0 +1,13 @@
+package com.cedrus.aeolion.kafkaspringpong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaspringpongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaspringpongApplication.class, args);
+	}
+
+}
